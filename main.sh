4db2a2c11ecdc64a109c8295b7195cd39d0d538e
Rahul Gupta <tankasalarahulgupta@gmail.com>
2020-01-28 09:26:53 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 docker-compose down
-echo "dockeropenmpi_mpi_head_1">get_hosts.sh.txt
+echo "docker-clusteropenmpi_head_1">get_hosts.sh.txt
 NNODES=3
 for (( c=1; c<=${NNODES}; c++ ))
 do
 
-   echo "dockeropenmpi_mpi_node_$c">>get_hosts.sh.txt
+   echo "docker-cluster-openmpi_mpi_node_$c">>get_hosts.sh.txt
 done
 
 docker build -t openmpi .
-docker network create dockeropenmpi_net
+docker network create docker-cluster-openmpi_net
 docker-compose scale mpi_head=1 mpi_node=${NNODES}
-docker cp dockeropenmpi_mpi_head_1:/home/mpirun/machines get_hosts.sh.txt
-docker exec -u mpirun dockeropenmpi_mpi_head_1 mpirun -hostfile machines ./helloworld>slurm-${SLURM_JOBID}.out
+docker cp docker-cluster-openmpi_mpi_head_1:/home/mpirun/machines get_hosts.sh.txt
+docker exec -u mpirun docker-cluster-openmpi_mpi_head_1 mpirun -hostfile machines ./helloworld>slurm-${SLURM_JOBID}.out
